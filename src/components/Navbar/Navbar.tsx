9add4eb4c2384b01a6f6b0c45009fc4d73cf973d
Alik_Avizha <aleksandravizha@icloud.com>
2023-03-04 19:58:39 +0300

    Work with route, browser-router

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={`${classes.item} ${classes.active}`}>
-                <a>Messages</a>
+                <a href="/dialogs">Messages</a>
             </div>
             <div className={classes.item}>
-                <a>News</a>
+                <a href="/news">News</a>
             </div>
             <div className={classes.item}>
-                <a>Music</a>
+                <a href="/music">Music</a>
             </div>
             <div className={classes.item}>
-                <a>Settings</a>
+                <a href="/settings">Settings</a>
             </div>
         </nav>
     )
