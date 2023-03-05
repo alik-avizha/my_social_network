290dd37cf7284242eaf69138a2f19cffb6dbeda7
Alik_Avizha <aleksandravizha@icloud.com>
2023-03-05 17:23:06 +0300

    Work with Dialogs Page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,20 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog+ ' ' + classes.active}>Aleksandr</div>
+                <div className={classes.dialog}>Denis</div>
+                <div className={classes.dialog}>Nikita</div>
+                <div className={classes.dialog}>Ivan</div>
+                <div className={classes.dialog}>Viktor</div>
+                <div className={classes.dialog}>Stepan</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello friend</div>
+                <div className={classes.message}>How are you do?</div>
+                <div className={classes.message}>Byi!</div>
+            </div>
         </div>
     );
 };
