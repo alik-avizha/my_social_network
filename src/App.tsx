f75841b66e7093d011b34715145e45a5b69eb6b4
Alik_Avizha <aleksandravizha@icloud.com>
2023-03-11 16:50:01 +0300

    moved BrowserRouter to Index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
@@ -16,8 +16,8 @@ type AppPropsType = {
 }
 
 const App = (props: AppPropsType) => {
+
     return (
-        <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
@@ -31,7 +31,6 @@ const App = (props: AppPropsType) => {
                     <Route path="/settings" render={()=><Settings/>}/>
                 </div>
             </div>
-        </BrowserRouter>
     );
 }
 export default App;
