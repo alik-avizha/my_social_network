d2b5789a85c2a815b9cb95a41c3f2e2fdb20ae2e
Alik_Avizha <aleksandravizha@icloud.com>
2023-04-22 16:23:53 +0300

    work with extends React.Component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -11,42 +11,41 @@ type UsersPropsType = {
     setUsers: (users: UserType[]) => void
 }
 
+export class Users extends React.Component<UsersPropsType> {
 
-export const Users = (props: UsersPropsType) => {
-
-    let getUsers = () => {
-        if (props.users.length === 0 ) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items)
-            })
-        }
+    constructor(props: UsersPropsType) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
-    const followHandler = (userId: number) => {
-        props.follow(userId)
+    followHandler = (userId: number) => {
+        this.props.follow(userId)
     }
-    const unfollowHandler = (userId: number) => {
-        props.unfollow(userId)
+    unfollowHandler = (userId: number) => {
+        this.props.unfollow(userId)
     }
 
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
-            {props.users.map(u => {
-                    return (
-                        <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {this.props.users.map(u => {
+                        return (
+                            <div key={u.id}>
                             <span>
                                 <div>
-                                     <img src={u.photos.small !== null ? u.photos.small : userPhoto } alt="avatar" className={styles.usersPhoto}/>
+                                     <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="avatar"
+                                          className={styles.usersPhoto}/>
                                 </div>
                                 <div>
                                     {u.followed
-                                        ? <button onClick={()=>unfollowHandler(u.id)}>UnFollow</button>
-                                        : <button onClick={()=>followHandler(u.id)}>Follow</button>
+                                        ? <button onClick={() => this.unfollowHandler(u.id)}>UnFollow</button>
+                                        : <button onClick={() => this.followHandler(u.id)}>Follow</button>
                                     }
                                 </div>
                             </span>
-                            <span>
+                                <span>
                                 <span>
                                     <div>{u.name}</div>
                                     <div>{u.status}</div>
@@ -56,10 +55,12 @@ export const Users = (props: UsersPropsType) => {
                                     <div>{'u.location.city'}</div>
                                  </span>
                             </span>
-                        </div>
-                    )
-                }
-            )}
-        </div>
-    );
-};
+                            </div>
+                        )
+                    }
+                )}
+            </div>
+        )
+    }
+}
+
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Users} from './Users';
 import {AppStateType} from '../../redux/redux-store';
 import {Dispatch} from 'redux';
 import {followAC, setUsersAC, unfollowAC, UserType} from '../../redux/users-reducer';
+import {Users} from './Users';
 
 let mapStateToProps = (state: AppStateType) => {
     return {
