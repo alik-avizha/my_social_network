591fae41942617f310b4761f74c2fb5d836dc6e0
Alik_Avizha <aleksandravizha@icloud.com>
2023-06-13 16:45:30 +0300

    work with hook useEffect

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 type ProfileStatusType = {
     status: string
@@ -9,6 +9,11 @@ export const ProfileStatusWithHooks: React.FC<ProfileStatusType> = (props) => {
 
     const [editMode, setEditMode] = useState<boolean>(false)
     const [title, setNewTitle] = useState<string>(props.status)
+
+    useEffect(()=>{
+        setNewTitle(props.status)
+    },[props.status ])
+
 
     const activateEditMode = () => {
         setEditMode(true)
