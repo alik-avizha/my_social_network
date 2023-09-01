36428dbf1752731b60afd98220165996633efd50
Alik_Avizha <aleksandravizha@icloud.com>
2023-09-01 22:13:16 +0300

    fix: chatPage

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ An online platform that is used for communication, dating, creating social relat
 There are several sections in the social network, the first one is 'Profile'. Here you will see information about users,
 and you can also change yours. There is also a feed of posts that the user can create.
 ![](src/assets/images/readme/mainPage.png)
+The dialogue page is reflected below. This page does not work with the API, but all actions are close to the real one
 ![](src/assets/images/readme/dialogsPage.png)
-
-More than 24,000 users have already registered on the social network. You can see their entire list in the "Users"
+The page below reflects how websockets work on examples of shared group chat. On this page you can send messages, share your thoughts, and have a good time
+![](src/assets/images/readme/chatPage.png)
+More than 24,900 users have already registered on the social network. You can see their entire list in the "Users"
 section. From there, you can go to their "Profile" and view their information.
 ![](src/assets/images/readme/usersPage.png)
 
--- src/components/chat/model/chat-reducer.ts
+++ src/components/chat/model/chat-reducer.ts
@@ -3,7 +3,10 @@ import {AppThunk} from "app/model/redux-store";
 import {Dispatch} from "redux";
 import {v1} from "uuid";
 
-export type ChatActionsType = ReturnType<typeof messagesReceivedActionCreator> | ReturnType<typeof statusChangedActionCreator>
+export type ChatActionsType =
+    | ReturnType<typeof messagesReceivedActionCreator>
+    | ReturnType<typeof statusChangedActionCreator>
+    | ReturnType<typeof clearDataActionCreator>
 export type InitialStateType = typeof initialState
 
 type ChatMessageType = ChatMessageApiType & {id: string}
@@ -19,6 +22,8 @@ export const chatReducer = (state: InitialStateType = initialState, action: Chat
             return {...state, messages: [...state.messages, ...action.payload.map(m => ({...m, id: v1()}))].filter((m, index, array) => index >= array.length - 100)}
         case "CHAT-PAGE/STATUS-CHANGED":
             return {...state, status: action.payload}
+        case "CHAT-PAGE/CLEAR-DATA":
+            return {...state, messages: []}
         default:
             return state
     }
@@ -31,6 +36,10 @@ export const messagesReceivedActionCreator = (messages: ChatMessageApiType[]) =>
 export const statusChangedActionCreator = (status: StatusType) => ({
     type: 'CHAT-PAGE/STATUS-CHANGED',
     payload: status
+}) as const
+
+export const clearDataActionCreator = () => ({
+    type: 'CHAT-PAGE/CLEAR-DATA'
 }) as const
 
 //ThunkCreators
@@ -60,6 +69,7 @@ export const startMessagesListeningThunkCreator = (): AppThunk => async (dispatc
     chatAPI.subscribe('status-changed', statusChangedHandlerCreator(dispatch))
 }
 export const stopMessagesListeningThunkCreator = (): AppThunk => async (dispatch) => {
+    dispatch(clearDataActionCreator())
     chatAPI.unsubscribe('message-received',newMessageHandlerCreator(dispatch))
     chatAPI.unsubscribe('status-changed', statusChangedHandlerCreator(dispatch))
     chatAPI.stop()
