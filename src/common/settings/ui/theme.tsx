b42300bdb2ad72113d460141961bf638a2706921
Alik_Avizha <aleksandravizha@icloud.com>
2023-09-06 22:19:03 +0300

    test: tabs and styles

--- src/common/settings/ui/theme.tsx
+++ src/common/settings/ui/theme.tsx
@@ -1,43 +1,44 @@
-import React, {useEffect} from 'react';
-import {AppStateType} from 'app/model/redux-store';
-import {useDispatch, useSelector} from 'react-redux';
-import {changeThemeTC, getThemeTC} from '../model/theme-reducer';
-import classes from './theme.module.css'
-import SuperSelect from '../../components/super-select/super-select';
+import { AppStateType } from "app/model/redux-store";
+import SuperSelect from "common/components/super-select/super-select";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getThemeTC, changeThemeTC } from "../model/theme-reducer";
+import classes from "./theme.module.css";
 
 const themes = [
-    {id: 1, value: 'light'},
-    {id: 2, value: 'dark'},
-]
+  { id: 1, value: "light" },
+  { id: 2, value: "dark" },
+];
 
 const Theme = () => {
+  const themeId = useSelector<AppStateType, number>(
+    (state) => state.theme.themeId
+  );
+  const dispatch = useDispatch();
 
-    const themeId =  useSelector<AppStateType, number>(state => state.theme.themeId)
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getThemeTC());
+  }, []);
 
-    useEffect(() => {
-        dispatch(getThemeTC())
-    }, [])
+  const change = (id: number) => {
+    dispatch(changeThemeTC(id));
+  };
 
-    const change = (id: number) => {
-        dispatch(changeThemeTC(id))
-    }
+  useEffect(() => {
+    document.documentElement.dataset.theme = themeId + "";
+  }, [themeId]);
 
-    useEffect(() => {
-        document.documentElement.dataset.theme = themeId + ''
-    }, [themeId])
-
-    return (
-        <div>
-            <SuperSelect
-                id={'hw12-select-theme'}
-                className={classes.select}
-                onChangeOption={change}
-                options={themes}
-                value={themeId}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <SuperSelect
+        id={"hw12-select-theme"}
+        className={classes.select}
+        onChangeOption={change}
+        options={themes}
+        value={themeId}
+      />
+    </div>
+  );
 };
 
 export default Theme;
--- src/components/chat/ui/chat-page.tsx
+++ src/components/chat/ui/chat-page.tsx
@@ -1,135 +1,162 @@
-import React, { ChangeEvent, FC, KeyboardEvent, memo, UIEvent, useEffect, useRef, useState } from 'react';
-import s from './chat-page.module.css'
+import React, {
+  ChangeEvent,
+  FC,
+  KeyboardEvent,
+  memo,
+  UIEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import s from "./chat-page.module.css";
 import { Button } from "common/components";
 import { Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import {
-    sendMessageThunkCreator,
-    startMessagesListeningThunkCreator,
-    stopMessagesListeningThunkCreator
+  sendMessageThunkCreator,
+  startMessagesListeningThunkCreator,
+  stopMessagesListeningThunkCreator,
 } from "components/chat/model/chat-reducer";
 import { AppStateType } from "app/model/redux-store";
 import { withAuthRedirect } from "common/hoc";
-import defaultPhoto from '../../../assets/images/user.jpg'
-
+import defaultPhoto from "../../../assets/images/user.jpg";
 
 const ChatPage = () => {
-
-    return (
-        <div className={s.chatWrapper}>
-            <Chat />
-        </div>
-    );
+  return (
+    <div className={s.chatWrapper}>
+      <Chat />
+    </div>
+  );
 };
 
 export default withAuthRedirect(ChatPage);
 
 const Chat = () => {
-    const status = useSelector((state: AppStateType) => state.chat.status)
-    const dispatch = useDispatch()
+  const status = useSelector((state: AppStateType) => state.chat.status);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(startMessagesListeningThunkCreator());
+    return () => {
+      dispatch(stopMessagesListeningThunkCreator());
+    };
+  }, []);
 
-    useEffect(() => {
-        dispatch(startMessagesListeningThunkCreator())
-        return () => {
-            dispatch(stopMessagesListeningThunkCreator())
-        }
-    }, [])
-
-
-    return (
-        <div>
-            {status === 'error' && <div>Some error occurred. Please refresh the page</div>}
-            <>
-                <ChatMessages/>
-                <AddIMessageForm/>
-            </>
-        </div>
-    )
-}
+  return (
+    <div>
+      {status === "error" && (
+        <div>Some error occurred. Please refresh the page</div>
+      )}
+      <>
+        <ChatMessages />
+        <AddIMessageForm />
+      </>
+    </div>
+  );
+};
 
 const ChatMessages = () => {
-    const messagesAnchorRef = useRef<HTMLDivElement>(null)
-    const messages = useSelector((state: AppStateType) => state.chat.messages)
-    const [isAutoScrollIsActive, setIsAutoScrollIsActive] = useState(true)
+  const messagesAnchorRef = useRef<HTMLDivElement>(null);
+  const messages = useSelector((state: AppStateType) => state.chat.messages);
+  const [isAutoScrollIsActive, setIsAutoScrollIsActive] = useState(true);
 
-    useEffect(() => {
-        if (isAutoScrollIsActive) {
-            messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'})
-        }
-    }, [messages])
-
-    const onScrollHandler = (e: UIEvent<HTMLDivElement>) => {
-        const element = e.currentTarget
-        if (Math.abs((element.scrollHeight - element.scrollTop) - element.clientHeight) < 300) {
-            !isAutoScrollIsActive && setIsAutoScrollIsActive(true)
-        } else {
-            isAutoScrollIsActive && setIsAutoScrollIsActive(false)
-        }
+  useEffect(() => {
+    if (isAutoScrollIsActive) {
+      messagesAnchorRef.current?.scrollIntoView({ behavior: "smooth" });
     }
+  }, [messages]);
 
-    return (
-        <div className={s.messages} onScroll={onScrollHandler}>
-            {messages?.map((m) => <ChatMessage key={m.id} photo={m.photo} userName={m.userName}
-                message={m.message} />)}
-            <div ref={messagesAnchorRef}></div>
-        </div>
-    )
-}
+  const onScrollHandler = (e: UIEvent<HTMLDivElement>) => {
+    const element = e.currentTarget;
+    if (
+      Math.abs(
+        element.scrollHeight - element.scrollTop - element.clientHeight
+      ) < 300
+    ) {
+      !isAutoScrollIsActive && setIsAutoScrollIsActive(true);
+    } else {
+      isAutoScrollIsActive && setIsAutoScrollIsActive(false);
+    }
+  };
+
+  return (
+    <div className={s.messages} onScroll={onScrollHandler}>
+      {messages?.map((m) => (
+        <ChatMessage
+          key={m.id}
+          photo={m.photo}
+          userName={m.userName}
+          message={m.message}
+        />
+      ))}
+      <div ref={messagesAnchorRef}></div>
+    </div>
+  );
+};
 
 export type ChatMessagePropsType = {
-    photo: string
-    userName: string
-    message: string
-}
-const ChatMessage: FC<ChatMessagePropsType> = memo(({ photo, userName, message }) => {
+  photo: string;
+  userName: string;
+  message: string;
+};
+const ChatMessage: FC<ChatMessagePropsType> = memo(
+  ({ photo, userName, message }) => {
     return (
-        <div className={s.message}>
-            <div className={s.imageAndText}>
-                <img className={s.avatar} alt={'myAvatar'}
-                    src={photo ? photo : defaultPhoto}
-                />
-                <div className={s.text}>
-                    <div className={s.name}>
-                        <Typography variant={"subtitle1"}>{userName}</Typography>
-                    </div>
-                    <pre className={s.messageText}>
-                        <Typography variant={"body2"}>{message}</Typography>
-                    </pre>
-                </div>
+      <div className={s.message}>
+        <div className={s.imageAndText}>
+          <img
+            className={s.avatar}
+            alt={"myAvatar"}
+            src={photo ? photo : defaultPhoto}
+          />
+          <div className={s.text}>
+            <div className={s.name}>
+              <Typography variant={"subtitle1"}>{userName}</Typography>
             </div>
+            <pre className={s.messageText}>
+              <Typography variant={"body2"}>{message}</Typography>
+            </pre>
+          </div>
         </div>
-    )
-})
+      </div>
+    );
+  }
+);
 
 export const AddIMessageForm = () => {
+  const status = useSelector((state: AppStateType) => state.chat.status);
+  const [value, setValue] = useState("");
 
-    const status = useSelector((state: AppStateType) => state.chat.status)
-    const [value, setValue] = useState('')
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.currentTarget.value);
+  };
 
-
-    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        setValue(e.currentTarget.value)
+  const addNewMessage = () => {
+    if (!value) {
+      return;
     }
+    dispatch(sendMessageThunkCreator(value));
+    setValue("");
+  };
+  const onKeyPressHandler = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    event.key === "Enter" && addNewMessage();
+  };
 
-    const addNewMessage = () => {
-        if (!value) {
-            return
-        }
-        dispatch(sendMessageThunkCreator(value))
-        setValue('')
-    }
-    const onKeyPressHandler = (event: KeyboardEvent<HTMLTextAreaElement>) => {
-        event.key === 'Enter' && addNewMessage()
-    }
-
-    return (
-        <div className={s.addNewContentWrapper}>
-            <textarea onKeyPress={onKeyPressHandler} placeholder={'send new message'}
-                value={value} onChange={onChangeHandler}/>
-            <Button name={'send'} callback={addNewMessage} disabled={status !== "ready"} />
-        </div>
-    );
+  return (
+    <div className={s.addNewContentWrapper}>
+      <textarea
+        onKeyPress={onKeyPressHandler}
+        placeholder={"send new message"}
+        value={value}
+        onChange={onChangeHandler}
+      />
+      <Button
+        name={"send"}
+        callback={addNewMessage}
+        disabled={status !== "ready"}
+      />
+    </div>
+  );
 };
