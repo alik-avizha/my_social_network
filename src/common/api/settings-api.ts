e109a25b31b13f129a30c395aeecf05f2ff532a0
Alik_Avizha <aleksandravizha@icloud.com>
2023-10-11 16:18:41 +0300

    fix: delete extra info

--- src/common/api/settings-api.ts
+++ src/common/api/settings-api.ts
@@ -4,9 +4,6 @@ import {ResultCodeForCaptcha, ResultCodesEnum} from '../enums';
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
-    headers: {
-        'API-KEY': '594e8ece-c3d0-41d5-adc9-eca0a193295b'
-    }
 });
 
 export type ResponseType<T = {}> = {
