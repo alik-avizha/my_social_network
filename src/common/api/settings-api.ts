b5a4b681f747c6b62b32919d41176eb795fa52fd
Alik_Avizha <aleksandravizha@icloud.com>
2023-10-17 15:20:18 +0300

    fix: test

--- src/common/api/settings-api.ts
+++ src/common/api/settings-api.ts
@@ -3,6 +3,9 @@ import {ResultCodeForCaptcha, ResultCodesEnum} from '../enums';
 
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '594e8ece-c3d0-41d5-adc9-eca0a193295b'
+    },
     withCredentials: true,
 });
 
